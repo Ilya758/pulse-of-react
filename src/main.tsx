56af372565080118e713b1531084419639505a5c
Illia Skaryna <subscriber6436@gmail.com>
2025-05-25 17:06:58 +0300

    fix: redirect for react-router

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app';
 
+(function () {
+  const redirect = sessionStorage.redirect;
+  delete sessionStorage.redirect;
+  if (redirect && redirect !== location.href) {
+    history.replaceState(null, '', redirect);
+  }
+})();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
