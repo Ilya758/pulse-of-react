8980512f88224c7aef6ac775ac046ca06d81ef1e
Illia Skaryna <subscriber6436@gmail.com>
2025-07-23 20:20:06 +0300

    fix: revert vite suppressing

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+// @ts-expect-error - __dirname is not defined
 import path from 'path';
 import svgr from 'vite-plugin-svgr';
 import { visualizer } from 'rollup-plugin-visualizer';
@@ -22,8 +23,11 @@ export default defineConfig(() => ({
   ],
   resolve: {
     alias: {
+      // @ts-expect-error - __dirname is not defined
       '@': path.resolve(__dirname, './src'),
+      // @ts-expect-error - __dirname is not defined
       '@assets': path.resolve(__dirname, './src/assets'),
+      // @ts-expect-error - __dirname is not defined
       '@components': path.resolve(__dirname, './src/components'),
     },
   },
