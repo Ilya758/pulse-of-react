10ed7e82c73b5076734d2a482da08ecbbe8c0333
Illia Skaryna <subscriber6436@gmail.com>
2025-11-01 22:56:47 +0300

    style(*): husky with commitlint

---
+++ commitlint.config.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  plugins: [
+    {
+      rules: {
+        'custom-regex': ({ header }) => [
+          /^(feat|fix|chore|docs|style|refactor|test)\((\*|[a-z]{3,})\): [a-z](?:[a-z-]*[a-z])+(?:\s+[a-z](?:[a-z-]*[a-z])+)*$/.test(
+            header,
+          ),
+          `Invalid format! Use:\n`
+            + `"type(scope): subject"\n`
+            + `- Type: feat|fix|chore|docs|style|refactor|test\n`
+            + `- Scope: "*" or lowercase letters (3+)\n`
+            + `- Subject: Lowercase words (3+ letters each, single spaces)`,
+        ],
+      },
+    },
+  ],
+  rules: {
+    'custom-regex': [2, 'always'],
+    'header-trim': [2, 'always', { chars: 'both' }],
+    'scope-case': [0],
+    'subject-case': [0],
+    'type-case': [0],
+    'type-enum': [0],
+  },
+};
